cdf9c3bc998c5c59827608b05afce73be99f2e07
Matt Gallucci <mattgallucci97@gmail.com>
2022-03-14 14:26:20 -0700

    mongoDB connected, node modules ignored again

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -3,11 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// connect to MongoDB
+const mongoose = require('mongoose');;
+mongoose.connect(process.env.MONGO_CONNECTION_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "MongoDB connection error: "));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
